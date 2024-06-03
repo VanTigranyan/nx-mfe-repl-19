131180e62db6f2b1c08a3f09c520a962ab30d25c
Van Tigranyan <vtigranyan@adobe.com>
2024-06-03 12:11:44 +0400

    chore: setup new MFEs

--- apps/shell/module-federation.config.ts
+++ apps/shell/module-federation.config.ts
@@ -14,7 +14,7 @@ const config: ModuleFederationConfig = {
    * declare module 'my-external-remote';
    *
    */
-  remotes: ['connections', 'users', 'login'],
+  remotes: [],
 };
 
 export default config;
--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,19 +1,19 @@
+import { loadRemoteModule } from "@nx/angular/mf";
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
     path: 'login',
-    loadChildren: () => import('login/Routes').then((m) => m.remoteRoutes),
+    loadChildren: () => loadRemoteModule('login', './Routes').then((m) => m.remoteRoutes),
   },
   {
     path: 'users',
-    loadChildren: () => import('users/Routes').then((m) => m.remoteRoutes),
+    loadChildren: () => loadRemoteModule('users', './Routes').then((m) => m.remoteRoutes),
   },
   {
     path: 'connections',
-    loadChildren: () =>
-      import('connections/Routes').then((m) => m.remoteRoutes),
+    loadChildren: () => loadRemoteModule('connections', './Routes').then((m) => m.remoteRoutes),
   },
   {
     path: '',
